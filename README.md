78f15f2ccc5752aee0ab475c57c7fba831ef201d
psmagicman <julienlaw1@gmail.com>
2013-07-09 17:03:39 -0700

    adding ajaxy shortcode and readme

---
+++ README.md
@@ -0,0 +1,6 @@
+# Ajaxy Shortcode
+-----------------
+
+A shortcode to use the Ajaxy Live Search plugin outside of widgets. The Ajaxy Live Search plugin must be enabled to use this. To use this plugin, paste the following shortcode anywhere in your WordPress site. \[ajaxy_search\]
+
+The [Ajaxy Live Search] (http://www.wordpress.org/plugins/ajaxy-search-form/) plugin is available on WordPress.
---
+++ ajaxy-live-search-shortcode.php
@@ -0,0 +1,22 @@
+<?php
+/*
+	Plugin Name: Ajaxy Shortcode
+	Plugin URI: https://github.com/ubc/ajaxy-shortcode 
+	Description: A shortcode to use the Ajaxy Live Search plugin outside of widgets. The Ajaxy Live Search plugin must be enabled to use this. 
+				 To use this plugin, paste the following shortcode anywhere in your WordPress site. [ajaxy_search]
+	Version: 1.0
+	Author: Julien Law
+	Author URI: https://github.com/ubc/ajaxy-shortcode
+	License: GPLv2 or later
+*/
+
+add_shortcode( 'ajaxy_search', 'ajaxy_shortcode' );
+
+function ajaxy_shortcode() {
+	if( function_exists( 'ajaxy_search_form' ) && class_exists( 'AjaxyLiveSearch' ) ) {
+		ajaxy_search_form();
+	}
+	else { ?>
+		<div id="ajaxy-live-search-shortcode">Please enable the <strong>Ajaxy Live Search</strong> plugin</div>		
+	<?php }
+}
